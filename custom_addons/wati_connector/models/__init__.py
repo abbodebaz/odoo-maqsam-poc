acdc5a4b5b0d7b4d8a37dbb7f817393434b9f7b4
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-08-31 00:27:06 +0300

    Load WATI conversation name guard

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -1,5 +1,6 @@
 from . import res_config_settings
 from . import wati_models
+from . import wati_name_guard
 from . import wati_send_fix
 from . import wati_assignment
 from . import wati_support_ticket
