ad48f97ebc639ef84eecb9d82ddb4ec825ae4129
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-01 15:57:07 +0300

    Load delivery-aware WATI automation dedupe fix

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -12,3 +12,4 @@ from . import wati_automation_builder
 from . import wati_name_guard
 from . import wati_automation_guard
 from . import wati_automation_channel_auto
+from . import wati_automation_dedup_fix
