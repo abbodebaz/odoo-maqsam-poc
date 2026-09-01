0449462832d7202540413a5940a7658b2879ed3c
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-01 21:44:42 +0300

    Register invoice WhatsApp integration model

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -4,6 +4,7 @@ from . import wati_assignment
 from . import wati_support_ticket
 from . import wati_crm_integration
 from . import wati_sale_integration
+from . import wati_account_integration
 from . import wati_send_fix
 from . import wati_automation
 from . import wati_automation_improvements
