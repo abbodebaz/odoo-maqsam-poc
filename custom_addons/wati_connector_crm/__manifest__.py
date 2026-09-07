c705188983b0db6e03ac79fca4285bcedc02820b
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-07 19:59:49 +0300

    Add standalone WATI CRM integration manifest

---
+++ custom_addons/wati_connector_crm/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "WATI Connector - CRM",
+    "version": "19.0.1.0.0",
+    "summary": "CRM integration for the WATI WhatsApp Connector",
+    "category": "Productivity",
+    "author": "Abdulrhman Bazarah",
+    "license": "LGPL-3",
+    "depends": ["wati_connector", "crm"],
+    "data": [
+        "views/crm_wati_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
