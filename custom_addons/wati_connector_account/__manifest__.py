7014d5e25c8417e0031d6cf0f758bfb681c8d97a
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-07 20:02:36 +0300

    Add standalone WATI Accounting integration manifest

---
+++ custom_addons/wati_connector_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "WATI Connector - Accounting",
+    "version": "19.0.1.0.0",
+    "summary": "Customer invoice integration for the WATI WhatsApp Connector",
+    "category": "Productivity",
+    "author": "Abdulrhman Bazarah",
+    "license": "LGPL-3",
+    "depends": ["wati_connector", "account"],
+    "data": [
+        "views/account_wati_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
