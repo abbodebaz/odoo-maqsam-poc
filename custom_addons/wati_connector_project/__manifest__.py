44f7e7d0e7983f984b3df30a0014183d7742d3b8
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-07 20:12:31 +0300

    Add Project integration addon manifest

---
+++ custom_addons/wati_connector_project/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "WATI Connector - Project",
+    "version": "19.0.1.0.0",
+    "summary": "Project task integration for the WATI WhatsApp Connector",
+    "category": "Productivity",
+    "author": "Abdulrhman Bazarah",
+    "license": "LGPL-3",
+    "depends": ["wati_connector", "project"],
+    "data": [
+        "views/project_wati_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
