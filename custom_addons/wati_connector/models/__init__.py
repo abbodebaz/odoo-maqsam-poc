074447eb0992f4696d6711af368b58737bb3ef9e
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-07 20:30:07 +0300

    Load WATI idempotency model

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -2,6 +2,7 @@ from . import res_config_settings
 from . import wati_models
 from . import wati_transport
 from . import wati_assignment
+from . import wati_idempotency
 from . import wati_support_ticket
 from . import wati_send_fix
 from . import wati_automation
