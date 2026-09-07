3a485a06190be121f621e0815ec0a5752e3d2f31
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-07 20:01:18 +0300

    Add standalone WATI Sales integration manifest

---
+++ custom_addons/wati_connector_sale/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "WATI Connector - Sales",
+    "version": "19.0.1.0.0",
+    "summary": "Sales and quotations integration for the WATI WhatsApp Connector",
+    "category": "Productivity",
+    "author": "Abdulrhman Bazarah",
+    "license": "LGPL-3",
+    "depends": ["wati_connector", "sale_management"],
+    "data": [
+        "views/sale_wati_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
