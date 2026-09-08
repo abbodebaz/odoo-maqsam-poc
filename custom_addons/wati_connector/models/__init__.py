912bfaf1138d4f7b168c90cc8c2a813a820af423
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-08 22:39:55 +0300

    Load smart automation target resolver

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -11,6 +11,7 @@ from . import wati_automation_improvements
 from . import wati_automation_ux
 from . import wati_automation_draft
 from . import wati_automation_builder
+from . import wati_automation_smart_target
 from . import wati_name_guard
 from . import wati_automation_guard
 from . import wati_automation_channel_auto
