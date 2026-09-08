665569f59d0a31b1cbcb84011823378e533d246d
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-08 23:43:59 +0300

    Load template switch hard-replace fix

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -15,6 +15,7 @@ from . import wati_automation_smart_target
 from . import wati_automation_recipient
 from . import wati_automation_template_mapper
 from . import wati_automation_template_truth
+from . import wati_automation_template_switch
 from . import wati_name_guard
 from . import wati_automation_guard
 from . import wati_automation_channel_auto
