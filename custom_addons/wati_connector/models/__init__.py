5427ccba1b5a6fad07716891e644c3e865109efd
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-09 14:45:18 +0300

    Load template provider error guard

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -41,3 +41,4 @@ from . import wati_template_compat
 from . import wati_template_provider_policy
 from . import wati_template_variable_save
 from . import wati_template_submission_truth
+from . import wati_template_button_guard
