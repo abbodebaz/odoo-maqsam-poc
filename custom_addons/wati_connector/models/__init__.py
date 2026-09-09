c35899265a6544c95ff56ac6db53ca0d08945375
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-09 22:19:30 +0300

    Register universal timeline model

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -51,3 +51,4 @@ from . import wati_otp_bridge
 from . import wati_smart_button
 from . import wati_smart_button_compat
 from . import wati_smart_button_admin_visibility
+from . import wati_universal_timeline
