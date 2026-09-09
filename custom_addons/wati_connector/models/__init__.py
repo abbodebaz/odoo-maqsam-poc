45ffc3bc361e8469080affe5e2714e8ba7492644
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-09 15:06:33 +0300

    Load final template lifecycle truth layer

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -42,3 +42,4 @@ from . import wati_template_provider_policy
 from . import wati_template_variable_save
 from . import wati_template_submission_truth
 from . import wati_template_button_guard
+from . import wati_template_lifecycle_final
