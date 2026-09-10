81682a740effb813d056582dd7cfe14531e59f04
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-10 11:44:24 +0300

    Align Sales addon with 19.0.11.0.3

--- custom_addons/wati_connector_sale/__manifest__.py
+++ custom_addons/wati_connector_sale/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "WATI Connector - Sales",
-    "version": "19.0.11.0.2",
+    "version": "19.0.11.0.3",
     "summary": "Sales and quotations integration for the WATI WhatsApp Connector",
     "category": "Productivity",
     "author": "Abdulrhman Bazarah",
