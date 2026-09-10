72db36869442a8f44e8ed5898e666e626a9c609b
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-10 11:55:33 +0300

    Align Accounting addon with 19.0.11.0.4

--- custom_addons/wati_connector_account/__manifest__.py
+++ custom_addons/wati_connector_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "WATI Connector - Accounting",
-    "version": "19.0.11.0.3",
+    "version": "19.0.11.0.4",
     "summary": "Customer invoice integration for the WATI WhatsApp Connector",
     "category": "Productivity",
     "author": "Abdulrhman Bazarah",
