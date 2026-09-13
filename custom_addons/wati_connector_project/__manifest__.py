cba9e6bea22bf847e7d919c4499d1bba0655c90e
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-13 19:09:57 +0300

    Align WATI Project addon version

--- custom_addons/wati_connector_project/__manifest__.py
+++ custom_addons/wati_connector_project/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "WATI Connector - Project",
-    "version": "19.0.11.0.5",
+    "version": "19.0.11.0.6",
     "summary": "Project task integration for the WATI WhatsApp Connector",
     "category": "Productivity",
     "author": "Abdulrhman Bazarah",
