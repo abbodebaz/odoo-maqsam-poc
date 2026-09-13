24fa71f4cd1e590a02a706b003ef9f3211c15b80
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-13 18:46:25 +0300

    Load OTP variable binding guard

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -52,6 +52,7 @@ from . import wati_message_identity
 from . import wati_otp_bridge
 from . import wati_otp_flow
 from . import wati_otp_flow_defaults
+from . import wati_otp_binding_guard
 from . import wati_smart_button
 from . import wati_smart_button_compat
 from . import wati_smart_button_admin_visibility
