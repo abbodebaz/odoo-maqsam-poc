763c9abdcf728c9ca88477634e88307cf67a0c2d
abbodebaz <115931577+abbodebaz@users.noreply.github.com>
2026-09-13 18:02:42 +0300

    Load managed OTP flow models

--- custom_addons/wati_connector/models/__init__.py
+++ custom_addons/wati_connector/models/__init__.py
@@ -50,6 +50,7 @@ from . import wati_template_lifecycle
 from . import wati_template_lifecycle_provider_scope
 from . import wati_message_identity
 from . import wati_otp_bridge
+from . import wati_otp_flow
 from . import wati_smart_button
 from . import wati_smart_button_compat
 from . import wati_smart_button_admin_visibility
